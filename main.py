324be0354ae9583470d2e1636f42a88e60eba0b6
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-02 20:08:03 +1000

    Added new question instance to questions list

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# from data import questions_data
+from data import questions_data
+from question import Question
+
+questions = []
+for question in questions_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    questions.append(new_question)
+
+print(questions)

6daa4c9938cb394e7422040925d4ea9ae626a7bf
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-02 22:16:19 +1000

    Created button using canvas frame

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk, Image
 from quiz_logic import QuizLogic
 
 THEME_COLOR = "#6fa3f7"
@@ -9,7 +10,7 @@ class QuizInterface:
         self.quiz = quiz_brain
         
         self.window = Tk()
-        self.window.title("Quizzler")
+        self.window.title("Kazoot")
         self.window.config(padx=40, pady=40, bg=THEME_COLOR)
         
         # Canvas
@@ -21,24 +22,31 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", fg='white', bg=THEME_COLOR, font=('Arial', 18, 'normal'))
         self.score_label.grid(column=1, row=0)
 
-        # Option 1
-        self.option1 = Canvas(height=50, width=200, bg='blue', highlightthickness=0)
-        self.option1_text = self.option1.create_text(100,25,text="Test", font=FONT, fill='white')
-        self.option1.grid(column=0, row=2, padx=5, pady=5)
+        # Option 1 btn
+        blue_img = Image.open("./images/blue.png")
+        blue_img = blue_img.resize((200,50))
+        blue_img = ImageTk.PhotoImage(blue_img)
+        self.option1_btn = Button(image=blue_img, text="Test", font=FONT, fg='white', highlightthickness=0)
+        self.option1_btn.grid(column=0, row=2, padx=5, pady=5)
+        
+        # # Option 1
+        # self.option1 = Canvas(height=50, width=200, bg='blue', highlightthickness=0)
+        # self.option1_text = self.option1.create_text(100,25,text="", font=FONT, fill='white')
+        # self.option1.grid(column=0, row=2, padx=5, pady=5)
         
         # Option 2
         self.option2 = Canvas(height=50, width=200, bg='green', highlightthickness=0)
-        self.option2_text = self.option2.create_text(100,25,text="Test", font=FONT, fill='white')
+        self.option2_text = self.option2.create_text(100,25,text="", font=FONT, fill='white')
         self.option2.grid(column=1, row=2, padx=5, pady=5)
 
         # Option 3
         self.option3 = Canvas(height=50, width=200, bg='red', highlightthickness=0)
-        self.option3_text = self.option3.create_text(100,25,text="Test", font=FONT, fill='white')
+        self.option3_text = self.option3.create_text(100,25,text="", font=FONT, fill='white')
         self.option3.grid(column=0, row=3, padx=5, pady=5)
 
         # Option 4
         self.option4 = Canvas(height=50, width=200, bg='orange', highlightthickness=0)
-        self.option4_text = self.option4.create_text(100,25,text="Test", font=FONT, fill='white')
+        self.option4_text = self.option4.create_text(100,25,text="", font=FONT, fill='white')
         self.option4.grid(column=1, row=3, padx=5, pady=5)
 
         # # Green button
@@ -55,17 +63,17 @@ class QuizInterface:
 
         self.window.mainloop()
 
-    # def get_next_question(self):
-    #     if self.quiz.still_has_questions():
-    #         q_text = self.quiz.next_question()
-    #         self.score_label.config(text=f"Score: {self.quiz.score}")
-    #         self.canvas.itemconfig(self.question_text, text=q_text)
-    #         self.canvas.configure(bg='white')
-    #     else:
-    #         self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
-    #         self.canvas.config(bg='white')
-    #         self.green_btn.config(state='disabled')
-    #         self.red_btn.config(state='disabled')
+    def get_next_question(self):
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            self.canvas.itemconfig(self.question_text, text=q_text)
+            self.canvas.configure(bg='white')
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.canvas.config(bg='white')
+            self.green_btn.config(state='disabled')
+            self.red_btn.config(state='disabled')
 
     # def true(self):
     #     is_right = self.quiz.check_answer('True')

bf59f845d1f06ca7b2664d7499f954b60d233749
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-02 20:03:27 +1000

    Added question class as main question frame

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-# TODO:
-
-# Create a UI class with all the UI setup needed for Tkinter
-# Create Questions class
-# Create QuizLogic class
-
-# Have a data module for collection of data
+# from data import questions_data

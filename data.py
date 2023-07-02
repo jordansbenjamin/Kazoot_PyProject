0d27bbebe307ea033af2fadfc81910ffb120a028
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-02 19:57:02 +1000

    Added one question as a model example

---
+++ data.py
@@ -0,0 +1,10 @@
+questions_data = [
+    "question": "Convert 1001 1010 to decimal",
+    "correct_answer": "154",
+    "incorrect_answer": [
+        "498",
+        "11",
+        "1100",
+        "145"
+    ]
+]
---
+++ main.py
@@ -0,0 +1,7 @@
+# TODO:
+
+# Create a UI class with all the UI setup needed for Tkinter
+# Create Questions class
+# Create QuizLogic class
+
+# Have a data module for collection of data

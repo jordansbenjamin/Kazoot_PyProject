2cd868a402c05bd2c058bd92dda730b5d12ad342
jordansbenjamin <14327@coderacademy.edu.au>
2023-07-03 14:56:33 +1000

    Adjusted UI window so it start centered on the screen

--- src/ui.py
+++ src/ui.py
@@ -18,12 +18,25 @@ class QuizInterface:
         
         self.window = Tk()
         self.window.title("Kazoot")
+        self.window.resizable(False,False)
+
+        window_height = 650
+        window_width = 670
+
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
+
+        x_cordinate = int((screen_width/2) - (window_width/2))
+        y_cordinate = int((screen_height/2) - (window_height/2))
+
+        self.window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
         self.window.config(padx=40, pady=40, bg=THEME_COLOR)
         
         # Canvas
         self.canvas = Canvas(height=300, width=450, bg='white', highlightthickness=0)
         self.question_text = self.canvas.create_text(225,110,text="Test", font=FONT, fill='black', width=350)
-        self.feedback_text = self.canvas.create_text(225,250,text="", font=FE_FONT, fill='black', width=280)
+        self.feedback_text = self.canvas.create_text(225,230,text="", font=FE_FONT, fill='black', width=280, justify='center')
         self.canvas.grid(column=0, row=1, columnspan=2, padx=20, pady=40)
 
         # Score label
@@ -32,7 +45,7 @@ class QuizInterface:
 
         # Option 1 btn
         blue_img = Image.open("./images/blue.png")
-        blue_img = blue_img.resize((260, 60))
+        blue_img = blue_img.resize((280, 60))
         blue_img = ImageTk.PhotoImage(blue_img)
         self.option1_btn = Button(font=BTN_FONT, fg='white', highlightthickness=0)
         self.option1_btn.config(image=blue_img, compound="center", text="Test", command=self.check_option_1)
@@ -40,7 +53,7 @@ class QuizInterface:
 
         # Option 2 btn
         green_img = Image.open("./images/green.png")
-        green_img = green_img.resize((260, 60))
+        green_img = green_img.resize((280, 60))
         green_img = ImageTk.PhotoImage(green_img)
         self.option2_btn = Button(font=BTN_FONT, fg='white', highlightthickness=0)
         self.option2_btn.config(image=green_img, compound="center", text="Test", command=self.check_option_2)
@@ -48,7 +61,7 @@ class QuizInterface:
 
         # Option 3 btn
         red_img = Image.open("./images/red.png")
-        red_img = red_img.resize((260, 60))
+        red_img = red_img.resize((280, 60))
         red_img = ImageTk.PhotoImage(red_img)
         self.option3_btn = Button(font=BTN_FONT, fg='white', highlightthickness=0)
         self.option3_btn.config(image=red_img, compound="center", text="Test", command=self.check_option_3)
@@ -56,7 +69,7 @@ class QuizInterface:
 
         # Option 4 btn
         orange_img = Image.open("./images/orange.png")
-        orange_img = orange_img.resize((260, 60))
+        orange_img = orange_img.resize((280, 60))
         orange_img = ImageTk.PhotoImage(orange_img)
         self.option4_btn = Button(font=BTN_FONT, fg='white', highlightthickness=0)
         self.option4_btn.config(image=orange_img, compound="center", text="Test", command=self.check_option_4)
@@ -112,10 +125,10 @@ class QuizInterface:
         answer = self.quiz.current_question.answer
         if is_right:
             self.canvas.configure(bg='green')
-            self.canvas.itemconfig(self.feedback_text, text=f"Correct! The answer is: {answer}")
+            self.canvas.itemconfig(self.feedback_text, text=f"Correct! The answer is:\n\n{answer}")
             self.window.after(2500, self.get_next_question)
         else:
             self.canvas.configure(bg='red')
-            self.canvas.itemconfig(self.feedback_text, text=f"Sorry, the correct answer is: {answer}")
+            self.canvas.itemconfig(self.feedback_text, text=f"Sorry, the correct answer is:\n\n{answer}")
             self.window.after(4000, self.get_next_question)
         
